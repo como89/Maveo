30843ff77be96281f452a022b179bbf7906493b4
como89 <como89.dev@gmail.com>
2016-09-12 21:20:42 -0400

    Tests unitaire
    Ajout des tests de la méthode arrêter et pause.

--- src/test/java/ca/qc/bdeb/maveo/modele/GestionnaireMusiqueTest.java
+++ src/test/java/ca/qc/bdeb/maveo/modele/GestionnaireMusiqueTest.java
@@ -8,6 +8,8 @@ import org.junit.Assert;
  */
 public class GestionnaireMusiqueTest {
 
+    private static final String TXT_LINK_MUSIQUE_TEST = "res/Gaara.mp3";
+
     static {
         UtilLib.chargerLibSysteme();
     }
@@ -15,12 +17,24 @@ public class GestionnaireMusiqueTest {
     @org.junit.Test
     public void demarer() throws Exception {
         GestionnaireMusique gestionMusique = new GestionnaireMusique();
-        boolean success = gestionMusique.demarrer("res/Gaara.mp3");
-        try {
-            Thread.currentThread().join(10000);
-        } catch (InterruptedException e) {
-        }
+        boolean success = gestionMusique.demarrer(TXT_LINK_MUSIQUE_TEST);
         Assert.assertTrue(success);
     }
 
+    @org.junit.Test
+    public void arreter() throws Exception {
+        GestionnaireMusique gestionMusique = new GestionnaireMusique();
+        gestionMusique.demarrer(TXT_LINK_MUSIQUE_TEST);
+        gestionMusique.arreter();
+        Assert.assertFalse(gestionMusique.enCoursDeLecture());
+    }
+
+    @org.junit.Test
+    public void pause() throws Exception {
+        GestionnaireMusique gestionMusique = new GestionnaireMusique();
+        gestionMusique.demarrer(TXT_LINK_MUSIQUE_TEST);
+        gestionMusique.pause();
+        Thread.currentThread().join(10);
+        Assert.assertFalse(gestionMusique.enCoursDeLecture());
+    }
 }
