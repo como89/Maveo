e170bc0f2c7a2629be69ae4aa625ee9d825e4324
Erokamisama <cedric.wutchanki1991@gmail.com>
2016-09-15 15:34:40 -0400

    Ajout des id du fxml au code

---
+++ src/main/java/ca/qc/bdeb/maveo/vue/Fenetre.java
@@ -0,0 +1,12 @@
+package ca.qc.bdeb.maveo.vue;
+
+/**
+ * Created by WuTchanKi on 2016-09-14.
+ */
+public class Fenetre {
+
+    public void afficherMessage(){
+        System.out.println("Ca marche");
+
+    }
+}
