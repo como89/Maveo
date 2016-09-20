ee37d155d9ade11a458dcab991b1d236162d83d2
Erokamisama <cedric.wutchanki1991@gmail.com>
2016-09-20 15:18:14 -0400

    Ajout de 3 labels lblProgression lblTxtVolume lblNomMedia

--- src/test/java/ca/qc/bdeb/maveo/vue/MainFrameTest.java
+++ src/test/java/ca/qc/bdeb/maveo/vue/MainFrameTest.java
@@ -1,100 +1,112 @@
 package ca.qc.bdeb.maveo.vue;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
+import java.net.URL;
 
-import static org.junit.Assert.*;
 
 /**
- * Created by WuTchanKi on 2016-09-12.
+ * Created by Cedric Wu Tchan Ki on 2016-09-07.
  */
 public class MainFrameTest {
 
 
-    private int test = 0;
 
-    @Before
-    public void setUp() {
+    @FXML
+    Button boutonPlayPause;
+
+    @FXML
+    Button boutonArreter;
+
+    @FXML
+    Button boutonPrecedent;
+
+    @FXML
+    Button boutonSuivant;
+
+    @FXML
+    Slider sliderProgression;
+
+    @FXML
+    Slider sliderVolume;
+
+    @FXML
+    Pane panelControleur;
+
+    @FXML
+    Pane panelEcran;
+
+    @FXML
+    BorderPane fenetrePrincipale;
+
+    @FXML
+    MenuBar menuBar;
+
+    @FXML
+    MenuItem menuItemFile;
+
+    @FXML
+    MenuItem menuItemFileOpen;
+
+    @FXML
+    MenuItem menuItemEdit;
+
+    @FXML
+    MenuItem menuItemHelp;
+
+    @FXML
+    MenuItem menuItemFileClose;
+
+    @FXML
+    MenuItem menuItemEditDelete;
+
+    @FXML
+    MenuItem menuItemHelpAbout;
+
+    Window fenetreTest;
+
+
+public void MainFrameTest (){
+
+    fenetreTest = fenetrePrincipale.getScene().getWindow();
+
+}
+
+@Test
+public void verifierPresenceFenetre() {
+
+}
+
+    @Test
+    public void verifierBoutons() throws Exception {
+
 
     }
 
     @Test
-    public void testBoutonArreter() throws AWTException, InterruptedException {
-
-
-        //testFrame.getBtnArreter().setLocation(25, 25);
-
-        Robot bot = new Robot();
-        bot.mouseMove(26, 26);
-        bot.mousePress(InputEvent.BUTTON1_MASK);
-//add time between press and release or the input event system may
-//not think it is a click
-
-        bot.mouseRelease(InputEvent.BUTTON1_MASK);
-        try {
-            Thread.sleep(250);
-        } catch (InterruptedException e) {
-        }
-
-      /*  testFrame.getBtnArreter().addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                test = 5;
-            }
-        });*/
-
-        assertEquals(5, test);
-
+    public void addEventHandlerBtnPlay() throws Exception {
 
     }
 
     @Test
-    public void testBoutonPlay() throws AWTException {
-        // testFrame.getBtnJouerPause().setLocation(50, 50);
-
-        Robot bot = new Robot();
-        bot.mouseMove(52, 52);
-        bot.mousePress(InputEvent.BUTTON1_MASK);
-//add time between press and release or the input event system may
-//not think it is a click
-        try {
-            Thread.sleep(250);
-        } catch (InterruptedException e) {
-        }
-        bot.mouseRelease(InputEvent.BUTTON1_MASK);
-
-
-        /*testFrame.getBtnJouerPause().addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                test = 5;
-            }
-        });*/
-
-        assertEquals(5, test);
+    public void addEventHandlerOuvrirFichier() throws Exception {
 
     }
-
-
-    @Test
-    public void testPresenceLabel() {
-        //Object objet = testFrame.getFenetre();
-
-        // assertNotEquals(null, objet);
-    }
-
-    @Test
-    public void testPresencePanel() {
-        //  Object objet = testFrame.getContent();
-        // Object objet2 = testFrame.getContent2();
-        // assertNotEquals(null, objet2);
-        // assertNotEquals(null, objet);
-        //   assertNotEquals(objet, objet2);
-    }
-
 
 }
