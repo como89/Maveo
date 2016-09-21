7f4eb37fc5a78d2fa2454ea9062d981dd6a2695d
catadeluxe <catadeluxxxe@gmail.com>
2016-09-21 08:10:56 -0400

    Soumission de Tokyo.mp3

--- src/main/java/ca/qc/bdeb/maveo/controleur/MainFrameControleur.java
+++ src/main/java/ca/qc/bdeb/maveo/controleur/MainFrameControleur.java
@@ -10,6 +10,7 @@ import javafx.event.EventHandler;
 import javafx.stage.Window;
 import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
 import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter;
 import uk.co.caprica.vlcj.player.MediaPlayerEventListener;
 
 import java.io.File;
@@ -168,7 +169,7 @@ public class MainFrameControleur {
     /**
      * Classe qui implémente l'interface MediaPlayerEventListener pour avoir accès aux évènements nécessaires
      */
-    class LecteurMediaEventListener implements MediaPlayerEventListener {
+    class LecteurMediaEventListener extends MediaPlayerEventAdapter {
 
         /**
          * Lorsquela position du média a changé, le slider est ajusté en conséquence
@@ -197,181 +198,5 @@ public class MainFrameControleur {
             mainFrame.getBtnJouerPause().setText(mainFrame.STR_BOUTON_JOUER);
             isFreeMutexLockSlider = true;
         }
-
-
-        @Override
-        public void stopped(MediaPlayer mediaPlayer) {
-        }
-
-
-        @Override
-        public void mediaChanged(MediaPlayer mediaPlayer, libvlc_media_t libvlc_media_t, String s) {
-        }
-
-        @Override
-        public void opening(MediaPlayer mediaPlayer) {
-        }
-
-        @Override
-        public void buffering(MediaPlayer mediaPlayer, float v) {
-        }
-
-        @Override
-        public void paused(MediaPlayer mediaPlayer) {
-        }
-
-        @Override
-        public void playing(MediaPlayer mediaPlayer) {
-        }
-
-        @Override
-        public void forward(MediaPlayer mediaPlayer) {
-        }
-
-        @Override
-        public void backward(MediaPlayer mediaPlayer) {
-        }
-
-
-
-        @Override
-        public void timeChanged(MediaPlayer mediaPlayer, long l) {
-        }
-
-        @Override
-        public void seekableChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void pausableChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void titleChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void snapshotTaken(MediaPlayer mediaPlayer, String s) {
-
-        }
-
-        @Override
-        public void lengthChanged(MediaPlayer mediaPlayer, long l) {
-
-        }
-
-        @Override
-        public void videoOutput(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void scrambledChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void elementaryStreamAdded(MediaPlayer mediaPlayer, int i, int i1) {
-
-        }
-
-        @Override
-        public void elementaryStreamDeleted(MediaPlayer mediaPlayer, int i, int i1) {
-
-        }
-
-        @Override
-        public void elementaryStreamSelected(MediaPlayer mediaPlayer, int i, int i1) {
-
-        }
-
-        @Override
-        public void corked(MediaPlayer mediaPlayer, boolean b) {
-
-        }
-
-        @Override
-        public void muted(MediaPlayer mediaPlayer, boolean b) {
-
-        }
-
-        @Override
-        public void volumeChanged(MediaPlayer mediaPlayer, float v) {
-
-        }
-
-        @Override
-        public void audioDeviceChanged(MediaPlayer mediaPlayer, String s) {
-
-        }
-
-        @Override
-        public void chapterChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void error(MediaPlayer mediaPlayer) {
-
-        }
-
-        @Override
-        public void mediaMetaChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void mediaSubItemAdded(MediaPlayer mediaPlayer, libvlc_media_t libvlc_media_t) {
-
-        }
-
-        @Override
-        public void mediaDurationChanged(MediaPlayer mediaPlayer, long l) {
-
-        }
-
-        @Override
-        public void mediaParsedChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void mediaFreed(MediaPlayer mediaPlayer) {
-
-        }
-
-        @Override
-        public void mediaStateChanged(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void mediaSubItemTreeAdded(MediaPlayer mediaPlayer, libvlc_media_t libvlc_media_t) {
-
-        }
-
-        @Override
-        public void newMedia(MediaPlayer mediaPlayer) {
-
-        }
-
-        @Override
-        public void subItemPlayed(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void subItemFinished(MediaPlayer mediaPlayer, int i) {
-
-        }
-
-        @Override
-        public void endOfSubItems(MediaPlayer mediaPlayer) {
-
-        }
     }
-
 }
