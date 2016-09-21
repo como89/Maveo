49821bbe9565253ef4bcc0acac0c89a61df2a4bf
Erokamisama <cedric.wutchanki1991@gmail.com>
2016-09-21 13:15:03 -0400

    Test mainFrames

--- src/test/java/ca/qc/bdeb/maveo/vue/MainFrameTest.java
+++ src/test/java/ca/qc/bdeb/maveo/vue/MainFrameTest.java
@@ -1,12 +1,8 @@
 package ca.qc.bdeb.maveo.vue;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,72 +13,13 @@ import static org.testfx.api.FxAssert.verifyThat;
 
 import java.net.URL;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by WuTchanKi on 2016-09-20.
  */
 public class MainFrameTest extends ApplicationTest {
 
-    @FXML
-    Button boutonPlayPause;
+    private MainFrame testFrame;
 
-    @FXML
-    Button boutonArreter;
-
-    @FXML
-    Button boutonPrecedent;
-
-    @FXML
-    Button boutonSuivant;
-
-    @FXML
-    Slider sliderProgression;
-
-    @FXML
-    Slider sliderVolume;
-
-    @FXML
-    Pane panelControleur;
-
-    @FXML
-    Pane panelEcran;
-
-    @FXML
-    BorderPane fenetrePrincipale;
-
-    @FXML
-    MenuBar menuBar;
-
-    @FXML
-    MenuItem menuItemFile;
-
-    @FXML
-    MenuItem menuItemFileOpen;
-
-    @FXML
-    MenuItem menuItemEdit;
-
-    @FXML
-    MenuItem menuItemHelp;
-
-    @FXML
-    MenuItem menuItemFileClose;
-
-    @FXML
-    MenuItem menuItemEditDelete;
-
-    @FXML
-    MenuItem menuItemHelpAbout;
-
-    @FXML
-    Label lblTxtVolume;
-
-    @FXML
-    Label lblNomMedia;
-
-    @FXML
-    Label lblProgression;
 
 
     @Override
@@ -90,10 +27,11 @@ public class MainFrameTest extends ApplicationTest {
         URL ressource = getClass().getClassLoader().getResource("GuiSample.fxml");
         FXMLLoader loader = new FXMLLoader(ressource);
         BorderPane page = loader.load();
+        testFrame = (MainFrame) loader.getController();
         Scene sceneTest = new Scene(page);
         stage.setScene(sceneTest);
         stage.setTitle(MainFrame.STR_NOM_PROGRAMME);
-        stage.show();
+       stage.show();
     }
 
     /**
@@ -101,7 +39,7 @@ public class MainFrameTest extends ApplicationTest {
      */
     @Test
     public void verifierExistenceBoutonPlay(){
-
+        Assert.assertEquals(testFrame.boutonPlayPause.getText(), testFrame.STR_BOUTON_JOUER);
     }
 
     /**
