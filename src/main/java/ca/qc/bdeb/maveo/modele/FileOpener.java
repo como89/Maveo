265ef2e582ef652137a0983e1c34eadb6b08abde
catadeluxe <catadeluxxxe@gmail.com>
2016-09-27 11:07:13 -0400

    Avancement extensions

--- src/main/java/ca/qc/bdeb/maveo/modele/FileOpener.java
+++ src/main/java/ca/qc/bdeb/maveo/modele/FileOpener.java
@@ -1,14 +1,19 @@
 package ca.qc.bdeb.maveo.modele;
 
 import javafx.stage.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.File;
+import java.io.FileReader;
 
 /**
  * Created by 1379708 on 2016-09-08.
  */
 public class FileOpener {
+
+    final String CHEMIN_FICHIER_EXTENSIONS = "src/main/java/ca.qc.bdev.maveo/modele/ExtensionsMaveo"; // pourrait être dans Target
 
     FileChooser fileChooser;
 
@@ -21,8 +26,28 @@ public class FileOpener {
 
         JSONParser parser = new JSONParser();
 
+        try {
+            Object obj = parser.parse(new FileReader(CHEMIN_FICHIER_EXTENSIONS));
 
 
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String name = (String) jsonObject.get("Name");
+            String author = (String) jsonObject.get("Author");
+            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+
+            System.out.println("Name: " + name);
+            System.out.println("Author: " + author);
+            System.out.println("\nCompany List:");
+
+          /*  Iterator<String> iterator = companyList.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
