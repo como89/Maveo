5b4df28362ccaf219cc380d020b044ad739849e0
Catalin <catadeluxxxe@gmail.com>
2016-11-13 13:47:14 -0500

    Refactoring Media.java -> ajustement classe pour la sauvegarde paroles dans fichier JSON

--- src/main/java/ca/qc/bdeb/maveo/modele/Media.java
+++ src/main/java/ca/qc/bdeb/maveo/modele/Media.java
@@ -6,11 +6,30 @@ package ca.qc.bdeb.maveo.modele;
 public class Media {
     private String titre;
     private String pathMedia;
+    private String parolesMedia;
 
-
-    public Media(String titre, String pathMedia){
+    /**
+     * Constructeur qui prend en paramètre le titre et le chemin du média
+     *
+     * @param titre     titre du média
+     * @param pathMedia chemin du média
+     */
+    public Media(String titre, String pathMedia) {
         this.titre = titre;
         this.pathMedia = pathMedia;
+    }
+
+    /**
+     * Constructeur qui prend en paramètre le titre, le chemin et les paroles du média
+     *
+     * @param titre        titre du média
+     * @param pathMedia    chemin du média
+     * @param parolesMedia paroles du média
+     */
+    public Media(String titre, String pathMedia, String parolesMedia) {
+        this.titre = titre;
+        this.pathMedia = pathMedia;
+        this.parolesMedia = parolesMedia;
     }
 
     public String getTitre() {
@@ -27,5 +46,13 @@ public class Media {
 
     public void setPathMedia(String pathMedia) {
         this.pathMedia = pathMedia;
+    }
+
+    public String getParolesMedia() {
+        return parolesMedia;
+    }
+
+    public void setParolesMedia(String parolesMedia) {
+        this.parolesMedia = parolesMedia;
     }
 }
