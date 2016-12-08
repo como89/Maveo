226005ec8980bb811fb12c030bff134255b18f5e
catadeluxe <catadeluxxxe@gmail.com>
2016-12-08 08:30:03 -0500

    Réglage bug: bouton afficher/cacher sous-titres ne lance désormais plus un NullPointerException si Maveo ne trouve pas des sous-titres

--- src/main/java/ca/qc/bdeb/maveo/controleur/MainFrameControleur.java
+++ src/main/java/ca/qc/bdeb/maveo/controleur/MainFrameControleur.java
@@ -219,7 +219,7 @@ public class MainFrameControleur {
                         } else {
                             Label label = mainFrame.getLblNomMedia();
                             URL ressource = getClass().getClassLoader().getResource("noart.png");
-                            Image image = SwingFXUtils.toFXImage(ImageIO.read(ressource),null);
+                            Image image = SwingFXUtils.toFXImage(ImageIO.read(ressource), null);
                             ImageView imageView = new ImageView(image);
                             label.setGraphic(imageView);
                         }
@@ -377,17 +377,14 @@ public class MainFrameControleur {
             // Si video
             if (GestionnaireFactory.getCurrentInstance() instanceof GestionnaireVideo) {
                 ((GestionnaireVideo) GestionnaireFactory.getCurrentInstance()).cacherAfficherSousTitres();
+
+                if (((GestionnaireVideo) GestionnaireFactory.getCurrentInstance()).isEstSousTitresDesactives()) {
+                    mainFrame.getMenuItemMediaSousTitres().setText("Afficher Sous Titres");
+                } else {
+
+                    mainFrame.getMenuItemMediaSousTitres().setText("Cacher Sous Titres");
+                }
             }
-
-            if(((GestionnaireVideo) GestionnaireFactory.getCurrentInstance()).isEstSousTitresDesactives()){
-                mainFrame.getMenuItemMediaSousTitres().setText("Afficher Sous Titres");
-            }
-            else{
-
-                mainFrame.getMenuItemMediaSousTitres().setText("Cacher Sous Titres");
-            }
-
-
         }
     }
 
@@ -397,7 +394,6 @@ public class MainFrameControleur {
             File file = fileOpener.activerOuvertureSousTitres(mainFrame.getFenetre());
 
             ((GestionnaireVideo) GestionnaireFactory.getCurrentInstance()).chargerSousTitres(file);
-
 
 
         }
